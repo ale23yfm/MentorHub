0b1e23f185d8c001229c7eb973386ffea2b97d9a
ale23yfm <aleifrim79@gmail.com>
2025-03-05 22:50:21 +0200

    added correct path for images, added some console.log for debugging

--- data/data.js
+++ data/data.js
@@ -6,42 +6,42 @@ const mentorsData = [
   },
   {
     name: "Gavriloaia Darius",
-    image: "../assets/Volunteers/Gavriloaia Darius.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Gavriloaia-Darius.jpg",
     subjects: ["Economie", "Public Speaking"],
   },
   {
     name: "Stolnicean Mirabela",
-    image: "../assets/Volunteers/Stolnicean-Mirabela.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Stolnicean-Mirabela.jpg",
     subjects: ["Vioară", "Biologie"],
   },
   {
     name: "Apopei Ștefan",
-    image: "../assets/Volunteers/Apopei-Stefan.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Apopei-Stefan.jpg",
     subjects: ["Logică", "Editare Video"],
   },
   {
     name: "Duceag Monica",
-    image: "../assets/Volunteers/Duceag Monica.png",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Duceag-Monica.png",
     subjects: ["Chimie", "Limba Engleză"],
   },
   {
     name: "Tablan Andrei Damian",
-    image: "../assets/Volunteers/Tablan Andrei Damian.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Tablan-Damian.jpg",
     subjects: ["C#", "Șah"],
   },
   {
     name: "Cîrcu Ștefan",
-    image: "../assets/Volunteers/Cîrcu Ștefan.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Cîrcu-Ștefan.jpg",
     subjects: ["T.I.C.", "Informatică"],
   },
   {
     name: "Necula Irene",
-    image: "../assets/Volunteers/Necula Irene.jpeg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Necula-Irene.jpeg",
     subjects: ["Filosofie", "Limba și Literatura Română"],
   },
   {
     name: "Vișan David",
-    image: "../assets/Volunteers/Vișan David.jpg",
+    image: "/home/ale/Desktop/Ale/MentorHub//assets/Volunteers/Vișan-David.jpg",
     subjects: ["Fizică", "Limba și Literatura Română"],
   },
 ];
