2a5737681968157be5e074d35dd388dbab9fc0af
ale23yfm <aleifrim79@gmail.com>
2025-03-09 20:54:50 +0200

    updates

--- data/data.js
+++ data/data.js
@@ -2,46 +2,46 @@ const mentorsData = [
   {
     "name": "Ifrim Alexandra",
     "image": "assets/Volunteers/Ifrim-Alexandra.jpg",
-    "subjects": ["PHP", "Matematică"],
+    "subjects": ["PHP", "Matematică"]
   },
   {
-    name: "Gavriloaia Darius",
-    image: "assets/Volunteers/Gavriloaia-Darius.jpg",
-    subjects: ["Economie", "Public Speaking"],
+    "name": "Gavriloaia Darius",
+    "image": "assets/Volunteers/Gavriloaia-Darius.jpg",
+    "subjects": ["Economie", "Public Speaking"]
   },
   {
-    name: "Stolnicean Mirabela",
-    image: "assets/Volunteers/Stolnicean-Mirabela.jpg",
-    subjects: ["Vioară", "Biologie"],
+    "name": "Stolnicean Mirabela",
+    "image": "assets/Volunteers/Stolnicean-Mirabela.jpg",
+    "subjects": ["Vioară", "Biologie"]
   },
   {
-    name: "Apopei Ștefan",
-    image: "assets/Volunteers/Apopei-Stefan.jpg", 
-    subjects: ["Logică", "Editare Video"],
+    "name": "Apopei Ștefan",
+    "image": "assets/Volunteers/Apopei-Stefan.jpg", 
+    "subjects": ["Logică", "Editare Video"]
   },
   {
-    name: "Duceag Monica",
-    image: "assets/Volunteers/Duceag-Monica.png",
-    subjects: ["Chimie", "Limba Engleză"],
+    "name": "Duceag Monica",
+    "image": "assets/Volunteers/Duceag-Monica.png",
+    "subjects": ["Chimie", "Limba Engleză"]
   },
   {
-    name: "Tablan Damian",
-    image: "assets/Volunteers/Tablan-Damian.jpg",
-    subjects: ["C#", "Șah"],
+    "name": "Tablan Damian",
+    "image": "assets/Volunteers/Tablan-Damian.jpg",
+    "subjects": ["C#", "Șah"]
   },
   {
-    name: "Cîrcu Ștefan",
-    image: "assets/Volunteers/Cîrcu-Ștefan.jpg",
-    subjects: ["T.I.C.", "Informatică"],
+    "name": "Cîrcu Ștefan",
+    "image": "assets/Volunteers/Cîrcu-Ștefan.jpg",
+    "subjects": ["T.I.C.", "Informatică"]
   },
   {
-    name: "Necula Irene",
-    image: "assets/Volunteers/Necula-Irene.jpeg",
-    subjects: ["Filosofie", "Limba și Literatura Română"],
+    "name": "Necula Irene",
+    "image": "assets/Volunteers/Necula-Irene.jpeg",
+    "subjects": ["Filosofie", "Limba și Literatura Română"]
   },
   {
-    name: "Vișan David",
-    image: "assets/Volunteers/Vișan-David.jpg",
-    subjects: ["Fizică", "Limba și Literatura Română"],
-  },
+    "name": "Vișan David",
+    "image": "assets/Volunteers/Vișan-David.jpg",
+    "subjects": ["Fizică", "Limba și Literatura Română"]
+  }
 ];
